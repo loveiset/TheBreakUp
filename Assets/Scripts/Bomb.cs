7231ebcdd89b744442c6839e5adecb5f6195c326
loveiset <loveiset@qq.com>
2014-05-05 14:43:21 +0800

    all done
    添加了音效
    添加了不同物体下落

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+    public GameObject prefab;
+    public int speed;
+    public AudioClip[] clips;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 trans = transform.position;
+        transform.position = new Vector3(trans.x, trans.y - speed * Time.deltaTime, trans.z);
+        if (trans.y < 0)
+        {
+            audio.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+            Instantiate(prefab, transform.position, Quaternion.identity);
+            transform.position = new Vector3(Random.Range(0, 60), 50, -16);
+        }
+	}
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+    float lastX;
+    bool isMoving = false;
+    public GameObject explosion;
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        animation.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //float halfW = Screen.width / 2;
+        Vector3 trans = transform.position;
+        transform.position = new Vector3((Input.mousePosition.x) / 20, trans.y, trans.z);
+        if (lastX != transform.position.x)
+        {
+            if (!isMoving)
+            {
+                isMoving = true;
+                if (!animation.IsPlaying("catch"))
+                {
+                    animation.Play("step");
+                }
+            }
+        }
+        else
+        {
+            if (isMoving)
+            {
+                isMoving = false;
+                if (!animation.IsPlaying("catch"))
+                {
+                    animation.Play("idle");
+                }
+            }
+        }
+        lastX = transform.position.x;
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        Vector3 trans = other.gameObject.transform.position;
+        if (other.gameObject.tag == "bomb")
+        {
+            Instantiate(explosion, trans, Quaternion.identity);
+        }
+        else if (other.gameObject.tag == "stein")
+        {
+            animation.Play("catch");
+        }
+        
+        other.gameObject.transform.position = new Vector3(Random.Range(0, 60), 50, trans.z);
+    }
+}
